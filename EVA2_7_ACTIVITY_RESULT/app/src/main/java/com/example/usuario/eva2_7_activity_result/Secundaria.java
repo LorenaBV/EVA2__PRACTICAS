f9912d0a2eee12e0e3f5487cc4b999cb831b398b
Rosa Lorena Bernal Velador <lore_velador@hotmail.com>
2017-10-16 12:08:45 -0600

    subi practica

---
+++ EVA2_7_ACTIVITY_RESULT/app/src/main/java/com/example/usuario/eva2_7_activity_result/Secundaria.java
@@ -0,0 +1,32 @@
+package com.example.usuario.eva2_7_activity_result;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Secundaria extends AppCompatActivity {
+    EditText edtTxtDatos;
+    Intent inResu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundaria);
+        edtTxtDatos=(EditText)findViewById(R.id.edtTxtDatos);
+        inResu=getIntent();
+
+    }
+
+    public void onClick(View v ){
+       String sCade = edtTxtDatos.getText().toString();
+        inResu.putExtra("MIS_DATOS",sCade);
+        setResult(Activity.RESULT_OK,inResu);
+        finish();
+
+
+    }
+}
